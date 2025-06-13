c32a85e6afaad1cc93d8a3285accb523d0fa32e2
Grace <40832036+GW122910@users.noreply.github.com>
2025-06-14 00:24:25 +0100

    Insert link moment.js to date and time of countries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-alert("Hello");
+function updateTime() {
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTimeMoment = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTimeMoment.format("dddd Do MMMM YYYY");
+  parisTimeElement.innerHTML = parisTimeMoment.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+
+  //HongKong
+  let hongkongElement = document.querySelector("#hongkong");
+  let hongkongDateElement = hongkongElement.querySelector(".date");
+  let hongkongTimeElement = hongkongElement.querySelector(".time");
+  let hongkongTimeMoment = moment().tz("Hongkong");
+  hongkongDateElement.innerHTML =
+    hongkongTimeMoment.format("dddd Do MMMM YYYY");
+  hongkongTimeElement.innerHTML = hongkongTimeMoment.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+
+  //India
+  let indiaElement = document.querySelector("#india");
+  let indiaDateElement = indiaElement.querySelector(".date");
+  let indiaTimeElement = indiaElement.querySelector(".time");
+  let indiaTimeMoment = moment().tz("India");
+  indiaDateElement.innerHTML = indiaTimeMoment.format("dddd Do MMMM YYYY");
+  indiaTimeElement.innerHTML = indiaTimeMoment.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
