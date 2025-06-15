1900571c0796b4e7517a1dce8ce4f3865e28a75a
Grace <40832036+GW122910@users.noreply.github.com>
2025-06-16 00:16:48 +0100

    Add javascript to city in dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,35 +1,62 @@
 function updateTime() {
   //Paris
   let parisElement = document.querySelector("#paris");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTimeMoment = moment().tz("Europe/Paris");
-  parisDateElement.innerHTML = parisTimeMoment.format("dddd Do MMMM YYYY");
-  parisTimeElement.innerHTML = parisTimeMoment.format(
-    "HH:mm:ss [<small>]A[</small>]"
-  );
-
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTimeMoment = moment().tz("Europe/Paris");
+    parisDateElement.innerHTML = parisTimeMoment.format("dddd Do MMMM YYYY");
+    parisTimeElement.innerHTML = parisTimeMoment.format(
+      "HH:mm:ss [<small>]A[</small>]"
+    );
+  }
   //HongKong
   let hongkongElement = document.querySelector("#hongkong");
-  let hongkongDateElement = hongkongElement.querySelector(".date");
-  let hongkongTimeElement = hongkongElement.querySelector(".time");
-  let hongkongTimeMoment = moment().tz("Hongkong");
-  hongkongDateElement.innerHTML =
-    hongkongTimeMoment.format("dddd Do MMMM YYYY");
-  hongkongTimeElement.innerHTML = hongkongTimeMoment.format(
-    "HH:mm:ss [<small>]A[</small>]"
-  );
-
+  if (hongkongElement) {
+    let hongkongDateElement = hongkongElement.querySelector(".date");
+    let hongkongTimeElement = hongkongElement.querySelector(".time");
+    let hongkongTimeMoment = moment().tz("Hongkong");
+    hongkongDateElement.innerHTML =
+      hongkongTimeMoment.format("dddd Do MMMM YYYY");
+    hongkongTimeElement.innerHTML = hongkongTimeMoment.format(
+      "HH:mm:ss [<small>]A[</small>]"
+    );
+  }
   //India
   let indiaElement = document.querySelector("#india");
-  let indiaDateElement = indiaElement.querySelector(".date");
-  let indiaTimeElement = indiaElement.querySelector(".time");
-  let indiaTimeMoment = moment().tz("India");
-  indiaDateElement.innerHTML = indiaTimeMoment.format("dddd Do MMMM YYYY");
-  indiaTimeElement.innerHTML = indiaTimeMoment.format(
-    "HH:mm:ss [<small>]A[</small>]"
-  );
+  if (indiaElement) {
+    let indiaDateElement = indiaElement.querySelector(".date");
+    let indiaTimeElement = indiaElement.querySelector(".time");
+    let indiaTimeMoment = moment().tz("Asia/Kolkata");
+    indiaDateElement.innerHTML = indiaTimeMoment.format("dddd Do MMMM YYYY");
+    indiaTimeElement.innerHTML = indiaTimeMoment.format(
+      "HH:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTimeMoment = moment().tz(cityTimeZone);
+  let citySelectUpdate = document.querySelector("#cities");
+  citySelectUpdate.innerHTML = `
+   <div class="place">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTimeMoment.format(
+              "dddd Do MMMM YYYY"
+            )}</div>
+          </div>
+          <div class="time">${cityTimeMoment.format(
+            "HH:mm:ss"
+          )}<small>${cityTimeMoment.format("A")}</small>
+          </div>
+        </div>
+  `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
